a19f2aac4335688cb651d64804ad9257fa4d5f08
akothari01 <aliasgar.a.kothari@gmail.com>
2025-01-29 10:18:28 -0700

    typing individual guesses
    user input won't be accepted until they hit enter after finishing a word

--- steamdle/src/App.jsx
+++ steamdle/src/App.jsx
@@ -4,10 +4,18 @@ import Keyboard from './components/Keyboard'
 
 export default function App() {
   const [wordToGuess, setWordToGuess] = useState("bayonetta")
-  const [guesses, setGuesses] = useState(Array(wordToGuess.length * 6).fill(' '))
+  const [guesses, setGuesses] = useState(Array(wordToGuess.length * 6).fill(''))
+  const [index, setIndex] = useState(0)
 
   function handleTyping(letter){
-    console.log(letter)
+    if(!(index % wordToGuess.length === 0) || index === 0){
+      setGuesses(prevGuess => {
+        const newGuess = [...prevGuess]
+        newGuess[index] = letter
+        return newGuess
+      })
+      setIndex(prevIndex => prevIndex + 1)
+    }
   }
 
   return (
