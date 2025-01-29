09e5542334c34c415afc8686ac498ea366eb86ed
akothari01 <aliasgar.a.kothari@gmail.com>
2025-01-29 10:07:44 -0700

    keyboard input handling
    keyboard can now type and letters are received

--- steamdle/src/App.jsx
+++ steamdle/src/App.jsx
@@ -4,11 +4,16 @@ import Keyboard from './components/Keyboard'
 
 export default function App() {
   const [wordToGuess, setWordToGuess] = useState("bayonetta")
-  const [guesses, setGuesses] = useState(Array(wordToGuess.length * 6).fill('-1'))
+  const [guesses, setGuesses] = useState(Array(wordToGuess.length * 6).fill(' '))
+
+  function handleTyping(letter){
+    console.log(letter)
+  }
+
   return (
     <main className='main-content'>
       <WordGuess gridSize={guesses}/>
-      <Keyboard/>
+      <Keyboard typing={handleTyping}/>
     </main>
   )
 }
--- steamdle/src/components/Keyboard.jsx
+++ steamdle/src/components/Keyboard.jsx
@@ -1,12 +1,14 @@
-export default function Keyboard(){
+export default function Keyboard(props){
     const keys = "1234567890qwertyuiopasdfghjklzxcvbnm"
     return(
         <section className="keyboard">
+            <button style={{backgroundColor:"#f54248"}}>DLT</button>
             {
                 keys.split('').map(letter => (
-                    <button>{letter}</button>
+                    <button onClick={() => (props.typing)(letter)} key={letter}>{letter}</button>
                 ))
             }
+            <button style={{backgroundColor: "#1df067"}}>ENT</button>
         </section>
     )
 }
