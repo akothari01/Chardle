fc34667fa901aeade2e10b20b68277517c3ce91a
akothari01 <aliasgar.a.kothari@gmail.com>
2025-01-30 16:55:17 -0700

    added a way to recognize game completion and drop confetti as well as accept keyboard input

--- steamdle/src/App.jsx
+++ steamdle/src/App.jsx
@@ -1,18 +1,22 @@
 import { useState } from 'react'
+import {useEffect} from 'react'
 import WordGuess from './components/WordGuess'
 import Keyboard from './components/Keyboard'
+import Confetti from 'react-confetti'
 
 export default function App() {
-  const [wordToGuess, setWordToGuess] = useState("bayonetta")
+  const [wordToGuess, setWordToGuess] = useState("sonic")
   const [guesses, setGuesses] = useState(Array(wordToGuess.length * 6).fill(''))
   const [index, setIndex] = useState(0)
   const[currentGuess, setCurrentGuess] = useState(1)
   const[guessCorrect, setGuessCorrect] = useState(Array(wordToGuess.length * 6).fill('#9cb7db'))
   const [gameWon, setGameWon] = useState(false)
-  const gameLost = index > wordToGuess.length*6-1
+  const gameLost = index >= wordToGuess.length*6+1
   const gameOver = gameWon || gameLost
 
   function handleTyping(letter){
+    console.log(letter)
+    if(!gameOver && index < wordToGuess.length*6){
     if(index < currentGuess * wordToGuess.length){
       setGuesses(prevGuess => {
         const newGuess = [...prevGuess]
@@ -20,23 +24,28 @@ export default function App() {
         return newGuess
       })
       setIndex(prevIndex => prevIndex + 1)
+    }
     } 
   }
 
   function deleteInput(){
-    const newIndex = (index < currentGuess*wordToGuess.length+1 && index > (currentGuess-1)*wordToGuess.length) ? index - 1 : index
-    setIndex(newIndex)
-    setGuesses(prevGuess => {
-      const newGuess = [...prevGuess]
-      newGuess[newIndex] = ''
-      return newGuess
-    })
+    if(!gameOver){
+      const newIndex = (index < currentGuess*wordToGuess.length+1 && index > (currentGuess-1)*wordToGuess.length) ? index - 1 : index
+      setIndex(newIndex)
+      setGuesses(prevGuess => {
+        const newGuess = [...prevGuess]
+        newGuess[newIndex] = ''
+        return newGuess
+      })
+    }
   }
 
   function enterInput(){
+    console.log(gameOver)
     if(!gameOver){
       if(index === currentGuess*wordToGuess.length){
         const guess = guesses
+        let gameStatus = true
         setGuessCorrect(prevCor => {
           const newCor = [...prevCor]
           const word = wordToGuess.split('')
@@ -46,11 +55,14 @@ export default function App() {
             }
             else if(word.includes(guess[i])){
               newCor[i] = '#ebcf34'
+              gameStatus = false
             }
             else{
               newCor[i] = '#747877'
+              gameStatus = false
             }
           }
+          setGameWon(gameStatus)
           return newCor
         })
         setCurrentGuess(prevGuess => prevGuess + 1)
@@ -58,9 +70,27 @@ export default function App() {
     }
   }
 
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if(event.key === "Enter"){
+        enterInput()
+      }
+      else if(event.key === "Backspace"){
+        deleteInput()
+      }
+      else if(event.key.match(/^[0-9a-z]+$/)){
+        handleTyping(event.key)
+      }
+    }
+    window.addEventListener('keydown', handleKeyPress)
+    return () =>{
+      window.removeEventListener('keydown', handleKeyPress);
+    }
+  }, [guesses, currentGuess])
 
   return (
     <main className='main-content'>
+      {gameWon ? <Confetti/> : undefined}
       <WordGuess gridSize={guesses} coloring={guessCorrect}/>
       <Keyboard typing={handleTyping} del={deleteInput} ent={enterInput}/>
     </main>
