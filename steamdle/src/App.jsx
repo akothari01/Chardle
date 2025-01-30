a85f354a35791405c172a461292534b153dcc4a3
akothari01 <aliasgar.a.kothari@gmail.com>
2025-01-30 11:39:13 -0700

    fixed delete button and typing
    added a new state to track the current guess and refactored the code to use that

--- steamdle/src/App.jsx
+++ steamdle/src/App.jsx
@@ -6,23 +6,23 @@ export default function App() {
   const [wordToGuess, setWordToGuess] = useState("bayonetta")
   const [guesses, setGuesses] = useState(Array(wordToGuess.length * 6).fill(''))
   const [index, setIndex] = useState(0)
+  const[currentGuess, setCurrentGuess] = useState(1)
 
   function handleTyping(letter){
-    if(!(index % wordToGuess.length === 0) || index === 0){
+    if(index < currentGuess * wordToGuess.length){
       setGuesses(prevGuess => {
         const newGuess = [...prevGuess]
         newGuess[index] = letter
         return newGuess
       })
       setIndex(prevIndex => prevIndex + 1)
-    }
+    } 
   }
 
   function deleteInput(){
     console.log(index)
-    console.log(!(index % wordToGuess.length === 0))
-    const newIndex = !(index % (wordToGuess.length-1) === 0) ? index - 1 : index
-    setIndex(prevIndex => !(index % (wordToGuess.length-1) === 0) ? prevIndex - 1 : prevIndex)
+    const newIndex = (index < currentGuess*wordToGuess.length+1) ? index - 1 : index
+    setIndex(newIndex)
     console.log(index)
     setGuesses(prevGuess => {
       const newGuess = [...prevGuess]
